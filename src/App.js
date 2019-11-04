090ed089e6836612f22f59866cffd87870e07dbf
github name <github email>
2019-11-04 11:41:56 -0300

    App ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { isAuthenticated } from "./infrastructure/auth";
+
+import { LoginPage, TaskPage } from "./components";
+
+class App extends Component {
+  state = { user: undefined };
+
+  update = user => {
+    this.setState({ user: user });
+  };
+
+  render = () => {
+    return (
+      <Container style={{ margin: 20 }}>
+        {!isAuthenticated() && <LoginPage onLogin={this.update} />}
+        {isAuthenticated() && <TaskPage />}
+      </Container>
+    );
+  };
 }
 
 export default App;
